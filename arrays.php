83b03f3663681e2fef5b7247d1514829b01772c7
Ariel <ariel.felippi@hotmail.com>
2026-05-05 22:00:32 -0300

    vetores somados

--- arrays.php
+++ arrays.php
@@ -2,21 +2,56 @@
 
 // Tipos Primitivos de dados: int, float, string, boolean, char.
 
-// Tipos de dados estruturados: array || vetor, objetos
+// Tipos de dados estruturados: array || vetor, objetos || classes.
 
 $numero = 10; // int
 $numero = 10.0; // float || double (decimal)
 
+// echo $numero; // 10 || 10.0
+
 $listaValores = array(); // array vazio
 $listaValores = []; // array vazio
 
-$listaValores = [10, 20, 3, 50];
-//                0,  1, 2,  3
+$listaValores = array(10, 20, 3, 50);
+$listaValores = [10, 20, 3, 50, 85]; // array com 6 elementos
+//                0,  1, 2,  3,  4
 
-$tam = count($listaValores); // 4
+// echo $listaValores[4]; // 85
 
-print_r($listaValores);
+$tamanhoArray = count($listaValores); // 5
 
-for ($i = 0; $i < $tam; $i++) {
-    echo $listaValores[$i] . "<br>";
+// print_r($listaValores);
+
+for ($i = 0; $i < $tamanhoArray; $i++) {
+    // echo "Indice(i): $i - Valor: " . $listaValores[$i] . "<br>";
+}
+
+/**
+ * Somar dois vetores e exibir o resultado.
+ * Exemplo:
+ * $vetor1 = [1, 2, 3];
+ *            0, 1, 2
+ * $vetor2 = [4, 5, 6];
+ * Resultado: [5, 7, 9] 
+ */
+
+$vetor1 = [1, 2, 3];
+$vetor2 = [4, 5, 6];
+$resultado = [];
+
+for ($i = 0; $i < count($vetor1); $i++) {
+    $valor1 = $vetor1[$i]; // 1
+    $valor2 = $vetor2[$i]; // 4
+
+    $soma = $valor1 + $valor2; // 1 + 4 = 5
+    
+    $resultado[$i] = $soma; // $resultado[$i] = 5
+
+    // $resultado[] = $vetor1[$i] + $vetor2[$i];
+}
+
+$tamanhoArray = count($resultado);
+
+for ($i = 0; $i < $tamanhoArray; $i++) {
+    echo "Indice(i): $i - Valor: " . $resultado[$i] . "<br>";
 }
