c6cd72eda34e77eea501b2bab1b6209dda6cf052
Ariel <ariel.felippi@hotmail.com>
2026-06-30 19:45:24 -0300

    criacao do CRUD

--- FuncionarioModel.php
+++ FuncionarioModel.php
@@ -11,15 +11,63 @@ class FuncionarioModel {
         $this->banco = $banco;
     }
 
-    public function listar()
+    public function listar($filtros = [], $limite = 20, $offset = 0)
     {
-        $sql = "SELECT * FROM {$this->tabela}";
+        $sql = "SELECT * FROM {$this->tabela}"; // SELECT * FROM funcionario 
+
+        if (!empty($filtros)) {
+            $where = [];
+
+            foreach ($filtros as $campo => $valor) {
+                $where[] = "$campo = '$valor'";
+            }
+            
+            $sql .= " WHERE " . implode(" AND ", $where);
+            /**
+             * SELECT * FROM funcionario WHERE nome  = 'Ariel' AND sobrenome = 'Silva';
+             */
+        }
+
+        $sql .= " LIMIT $limite OFFSET $offset";
+
         return $this->banco->execQuery($sql, "Não foi possivel obter os funcionarios.");
     }
 
-    /**
-     * Create.
-     * INSERT INTO funcionario (nome, sobrenome, salario, cargo, setor, cracha, idPessoa) VALUES( '', '', 0, '', '', '', NULL);
-    */
+    public function listarPorId($id)
+    {
+        $sql = "SELECT * FROM {$this->tabela} WHERE id = $id LIMIT 1";
+        return $this->banco->execQuery($sql, "Não foi possivel obter o funcionario.");
+    }
+
+    public function excluir($id)
+    {
+        $sql = "DELETE FROM {$this->tabela} WHERE id = $id";
+
+        return $this->banco->execQuery($sql, "Não foi possivel excluir o funcionario.");
+    }
+    
+    public function criar($dados) {
+
+        $campos = implode(", ", array_keys($dados));
+        $valores = implode("', '", array_values($dados));
+
+        $sql = "INSERT INTO {$this->tabela} ($campos) VALUES ('$valores')";
+
+        return $this->banco->execQuery($sql, "Não foi possivel criar o funcionario.");
+    }
+
+    public function atualizar($id, $dados) {
+        $set = [];
+
+        foreach ($dados as $campo => $valor) {
+            $set[] = "$campo = '$valor'";
+        }
+
+        $setString = implode(", ", $set);
+
+        $sql = "UPDATE {$this->tabela} SET $setString WHERE id = $id";
+
+        return $this->banco->execQuery($sql, "Não foi possivel atualizar o funcionario.");
+    }
 
 }
