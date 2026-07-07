211bc2b36e682f93218cfd0892bf5ea24e41ec35
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2026-07-07 18:16:13 -0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ require_once "conexao.php";
 require_once "BancoDeDados.php";
 require_once "FuncionarioModel.php";
 require_once "Funcionario.php";
+require_once "FuncionarioController.php";
 
 $banco = new BancoDeDados($conexao);
 $funcionarioModel = new FuncionarioModel($banco);   
