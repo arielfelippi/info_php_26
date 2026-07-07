432cbba4e509b7ba81b9c96e8a25eb94bf6ee69d
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2026-07-07 18:38:49 -0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,13 +2,13 @@
 
 session_start();
 
-require_once "conexao.php";
-require_once "BancoDeDados.php";
-require_once "FuncionarioModel.php";
-require_once "Funcionario.php";
-require_once "FuncionarioController.php";
+require_once __DIR__ . "/conexao.php";
+require_once __DIR__ . "/BancoDeDados.php";
+require_once __DIR__ . "/BaseModel.php";
+require_once __DIR__ . "/FuncionarioModel.php";
+require_once __DIR__ . "/Funcionario.php";
+require_once __DIR__ . "/FuncionarioController.php";
 
 $banco = new BancoDeDados($conexao);
 $funcionarioModel = new FuncionarioModel($banco);
-$funcionario = new Funcionario($funcionarioModel);
-$funcionarioController = new FuncionarioController($funcionario);
+$funcionarioController = new FuncionarioController($funcionarioModel);
